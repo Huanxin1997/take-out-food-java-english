5e8a69cd084300f1d5dd0f13ca06e509ee8171cd
Hans <741581879@qq.com>
2020-06-28 23:28:25 +0800

    no message

---
+++ src/main/java/StringUtils.java
@@ -0,0 +1,12 @@
+public class StringUtils {
+
+  public static final String BLANK = " ";
+
+    public static boolean isEmpty(String str) {
+        return null == str || "".equals(str);
+    }
+
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
+}
